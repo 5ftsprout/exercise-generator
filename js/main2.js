3967944d9a1da3e3cb9d5eb5a4461f53c2bf1328
Annie Chhan <125323803+5ftsprout@users.noreply.github.com>
2024-03-06 13:09:25 -0500

    first commit

---
+++ js/main2.js
@@ -0,0 +1,66 @@
+
+function generateCategoryList(){
+    let url = 'https://wger.de/api/v2/exercisecategory'
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            catList = document.getElementById('category_list')
+            data.results.forEach(i => {
+                createLi = document.createElement('li')
+                let exerciseId = i.id
+                catList.appendChild(createLi).setAttribute('id', exerciseId)
+                document.getElementById(`${exerciseId}`).innerText = i.name
+                document.getElementById(`${exerciseId}`).addEventListener('click', fetchExercise)
+            })
+        })
+        .catch(err => {
+            console.log(`Error $[err]`)
+        })
+
+}
+
+function fetchExercise() {
+    let url = 'https://wger.de/api/v2/exercise'
+    catId = this.getAttribute('id')
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        numOfExercises = data.count
+        randomPageNum = Math.floor(Math.random() * (numOfExercises))
+        console.log(randomPageNum)
+        generateExercise()
+    .catch(err => {
+        console.log(`Error ${err}`)
+    })    
+})
+
+function generateExercise() {
+    fetch(`${url}/?limit=50&offset=${randomPageNum}`)
+            .then(res => res.json())
+            .then(data => {
+                for (i=0; i < 50; i++)
+                    if (data.results[i].category == catId && data.results[i].language == '2') {
+                        console.log(data.results[i])
+                        exerciseData = data.results[i]
+                        document.getElementById('exercise_name').innerText = exerciseData.name
+                        document.getElementById('exercise_desc').innerText = exerciseData.description
+                        document.getElementById('exercise_img').style.width = `20em`
+                        document.getElementById('exercise_img').style.height = `20em`
+                        //document.getElementById('exercise_img').setAttribute('src', )
+                        document.getElementById('exercise_img').setAttribute('alt', 'Image demonstrating the workout.')
+                        return
+                    }
+                    else {
+                        console.log('Next')
+                        i++
+                    }
+                    console.log(`No match`)
+                    fetchExercise()
+            })
+            .catch(err => {
+                console.log(`Error ${err}`)
+            })
+    }
+}
+
+generateCategoryList()
